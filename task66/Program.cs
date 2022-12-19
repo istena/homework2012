97d8fff1ce8ca443930ab3e62084fb5b1268e860
Ilya <sannikov444@yandex.ru>
2022-12-20 01:25:01 +0300

    homework

---
+++ task66/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int rec(int m,int n)
+{
+    if (n==m) return n;
+    return rec(m+1,n)+m;
+}
+
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(m,n));
---
+++ task68/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int rec(int m,int n)
+{
+    if (m==0) return n+1;
+    if ((m>0)&&(n==0)) return rec(m-1,1);
+    return rec(m-1,rec(m,n-1));
+}
+
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(m,n));
